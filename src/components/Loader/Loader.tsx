995dd9d1ccddd6c104ac3a04c90360041151da38
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2025-04-26 21:16:15 +0100

    Replace react-loader-spinner with CSS Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,18 +1,7 @@
-import { Vortex } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 export const Loader = () => {
   return (
-    <div className={css.Loader}>
-      <Vortex
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="vortex-loading"
-        wrapperStyle={{}}
-        wrapperClass="vortex-wrapper"
-        colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
-      />
-    </div>
+    <div className={css.Loader}></div>
   );
 };
